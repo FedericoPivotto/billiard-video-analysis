007f3b74c7b81423f6ead068f89cda28f142489a
Federico Pivotto <federico.pivotto@studenti.unipd.it>
2024-06-25 22:31:55 +0200

    CMake baseline

---
+++ system/include/library.h
@@ -0,0 +1,8 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+// libraries required in this source file
+
+int function_name();
+
+#endif // LIBRARY_H
---
+++ system/src/library.cpp
@@ -0,0 +1,9 @@
+#include <library.h>
+
+// librarires required in this source file and not already included in library.h
+
+int function_name() {
+    int variable_name = 69;
+    
+    return variable_name;
+}
