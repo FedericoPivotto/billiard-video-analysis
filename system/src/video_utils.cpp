c8bd92e0e2dd268d2be310517f87f67fe01b2296
Federico Pivotto <federico.pivotto@studenti.unipd.it>
2024-06-29 17:10:15 +0200

    Add namespace vu:: and refactor constant

--- system/src/video_utils.cpp
+++ system/src/video_utils.cpp
@@ -8,27 +8,24 @@
 // filesystem: glob()
 #include <opencv2/core/utils/filesystem.hpp>
 
-// error constants
-#define VIDEO_READ_ERROR -1
-
-void get_video_paths(std::vector<cv::String>& video_paths) {
+void vu::get_video_paths(std::vector<cv::String>& video_paths) {
     // benchmark dataset video paths
     const cv::String dataset_dir("../dataset/"), video_path("game*_clip*.mp4");
     bool nested_dir = true;
     cv::utils::fs::glob(dataset_dir, video_path, video_paths, nested_dir);
 }
 
-void get_video_captures(const std::vector<cv::String> video_paths, std::vector<cv::VideoCapture>& captures) {
+void vu::get_video_captures(const std::vector<cv::String> video_paths, std::vector<cv::VideoCapture>& captures) {
     // read videos from dataset
     for(cv::String video_path : video_paths)
         captures.push_back(cv::VideoCapture(video_path));
 }
 
-void read_video_frames(cv::VideoCapture capture, std::vector<cv::Mat>& video_frames) {
+void vu::read_video_frames(cv::VideoCapture capture, std::vector<cv::Mat>& video_frames) {
     // safety check on video
     if(! capture.isOpened()){
         std::cerr << "Error: The video cannot be read"<< std::endl;
-        exit(VIDEO_READ_ERROR);
+        exit(vu::VIDEO_READ_ERROR);
     }
     
     // read video frames
@@ -39,7 +36,7 @@ void read_video_frames(cv::VideoCapture capture, std::vector<cv::Mat>& video_fra
     capture.release();
 }
 
-void show_video_frames(const std::vector<cv::Mat> video_frames) {
+void vu::show_video_frames(const std::vector<cv::Mat> video_frames) {
     // show video frames
     for(cv::Mat frame : video_frames) {
         cv::namedWindow("Frame");
