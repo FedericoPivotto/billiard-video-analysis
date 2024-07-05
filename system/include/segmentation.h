98ee368f63b55cf7d847bed84f542616829eb60a
Leonardo Egidati <leonardo.egidati@gmail.com>
2024-07-05 17:01:20 +0200

    separazione file per segmentation

--- system/include/segmentation.h
+++ system/include/segmentation.h
@@ -2,7 +2,14 @@
 #define SEGMENTATION_H
 
 // libraries required in this source file
+#include <iostream>
+// utility: std::pair
+#include <utility>
+// highgui: cv::Mat
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
 int function_name();
+//void ball_segmentation(od::Ball ball_bbox, cv::Mat &frame);
 
 #endif // SEGMENTATION_H
